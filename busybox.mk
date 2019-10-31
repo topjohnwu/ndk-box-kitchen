a14939aad5031afabd5faccff0a93fa6a2591598
topjohnwu <topjohnwu@gmail.com>
2019-10-31 17:45:49 -0400

    Update for 1.31.1

--- busybox.mk
+++ busybox.mk
@@ -7,9 +7,11 @@ LOCAL_MODULE := busybox
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 include $(LOCAL_PATH)/Android_src.mk
 
+LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 LOCAL_LDFLAGS := -static
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD \
--DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP
+-DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP \
+-D"wait3(status, option, rusage)=wait4(-1, status, option, rusage)"
 
 include $(BUILD_EXECUTABLE)
