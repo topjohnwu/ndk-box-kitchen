9cfb96768ae53222bb5104c1bf5d7c5c0d501e0b
osm0sis <osm0sis@outlook.com>
2023-09-11 14:34:41 -0300

    Fix SELinux in builds
    - Magisk's Application.mk uses lto_fix.lds, which was missing here, resulting in (at least) SELinux functionality being broken on direct ndk-box-kitchen builds of busybox

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,7 @@
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PLATFORM := android-26
 APP_CFLAGS := -Wall -Oz -fomit-frame-pointer -flto
-APP_LDFLAGS := -flto
+APP_LDFLAGS := -flto -T jni/lto_fix.lds
 
 ifeq ($(OS),Windows_NT)
 APP_SHORT_COMMANDS := true
