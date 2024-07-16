e6067b539fd55b88ed0efd1d8dec85d250384d98
topjohnwu <topjohnwu@gmail.com>
2024-07-16 08:54:46 -0700

    Update Application.mk

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,9 @@
-APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_PLATFORM := android-26
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64 riscv64
+APP_PLATFORM := android-35
 APP_CFLAGS := -Wall -Oz -fomit-frame-pointer -flto
-APP_LDFLAGS := -flto
+# Disable all security features
+APP_CFLAGS += -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector -U_FORTIFY_SOURCE
+APP_LDFLAGS := -flto -Wl,--icf=all
 
 ifeq ($(OS),Windows_NT)
 APP_SHORT_COMMANDS := true
