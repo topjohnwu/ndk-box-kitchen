effd7dbd351d5e8bfbc288c10efd1c0fe898cb0f
topjohnwu <topjohnwu@gmail.com>
2024-07-16 08:40:01 -0700

    Update patches and config

--- busybox.mk
+++ busybox.mk
@@ -5,11 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := busybox
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_STATIC_LIBRARIES := libselinux
-LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 LOCAL_LDFLAGS := -static -Wl,--wrap=realpath -Wl,--wrap=rename -Wl,--wrap=renameat
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD -D__USE_GNU \
--DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP \
--Wno-implicit-function-declaration
+-DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP
 
 LOCAL_SRC_FILES := \
