9fa83933e16e7067ec3cf3f0418ebfd2f5608dfd
topjohnwu <topjohnwu@gmail.com>
2024-07-17 11:10:24 -0700

    Add 16k page support

--- jni/Application.mk
+++ jni/Application.mk
@@ -4,6 +4,7 @@ APP_CFLAGS := -Wall -Oz -fomit-frame-pointer -flto
 # Disable all security features
 APP_CFLAGS += -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector -U_FORTIFY_SOURCE
 APP_LDFLAGS := -flto -Wl,--icf=all
+APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 
 ifeq ($(OS),Windows_NT)
 APP_SHORT_COMMANDS := true
