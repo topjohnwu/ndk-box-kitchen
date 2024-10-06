14d189ea3070a8167b3576bf83fe070d4a3441af
topjohnwu <topjohnwu@gmail.com>
2024-10-06 01:43:13 -0700

    Fix DNS issues on Bionic

--- busybox.mk
+++ busybox.mk
@@ -1,11 +1,11 @@
-MINORLEVEL := 0
+MINORLEVEL := 1
 BB_VER := "$(VERSION).$(PATCHLEVEL).$(SUBLEVEL).$(MINORLEVEL) topjohnwu"
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := busybox
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_STATIC_LIBRARIES := libselinux
-LOCAL_LDFLAGS := -static -Wl,--wrap=realpath -Wl,--wrap=rename -Wl,--wrap=renameat
+LOCAL_LDFLAGS := -static -Wl,--wrap=realpath -Wl,--wrap=rename -Wl,--wrap=renameat -Wl,--wrap=getaddrinfo
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD -D__USE_GNU \
 -DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP
